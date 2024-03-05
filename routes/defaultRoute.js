9f94e4a747cbe818a7293551ec4896bf31d35470
THUANPHAN\phang <thuanpg159@gmail.com>
2024-03-05 16:33:02 +0700

    test

--- routes/defaultRoute.js
+++ routes/defaultRoute.js
@@ -6,6 +6,12 @@ const { protect } = authController;
 const { getMe } = userController;
 const router = express.Router();
 
+router.route('/').get((req, res, next) => {
+  res.status(200).json({
+    status: 'Success',
+    message: 'Hello',
+  });
+});
 router.route('/me').get(protect, getMe);
 
 export default router;
